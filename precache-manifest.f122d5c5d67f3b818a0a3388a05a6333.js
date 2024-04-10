e693c36306035f1ab855e8e4d5b9b0f52d4a3efe
P. Y <papa.yaw@monterosa.co.uk>
2024-04-10 10:56:11 +0000

    Updates

---
+++ precache-manifest.f122d5c5d67f3b818a0a3388a05a6333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8504506f30a322ac2fdd987f4ba35c7c",
+    "url": "/pyofori/portfolio/index.html"
+  },
+  {
+    "revision": "d0391d209f9c5b1a6908",
+    "url": "/pyofori/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5787e9b0730745957f94",
+    "url": "/pyofori/portfolio/static/css/main.6c83694d.chunk.css"
+  },
+  {
+    "revision": "d0391d209f9c5b1a6908",
+    "url": "/pyofori/portfolio/static/js/2.ad327c53.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/pyofori/portfolio/static/js/2.ad327c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5787e9b0730745957f94",
+    "url": "/pyofori/portfolio/static/js/main.5511b1c2.chunk.js"
+  },
+  {
+    "revision": "a83ecca80ae7f7466763",
+    "url": "/pyofori/portfolio/static/js/runtime-main.b9677eef.js"
+  }
+]);
